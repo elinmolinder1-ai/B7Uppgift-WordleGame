3bfebb10da416e571e1569b27e481aa420ecf1df
elinmolindr <elinmolinder1@gmail.com>
2026-04-02 15:07:50 +0200

    Install ejs and set up highscore.ejs

--- backend/routes/pageRoutes.js
+++ backend/routes/pageRoutes.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
+import { getAllScores } from "../controllers/scoreController.js";
+
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Game route works");
+// About page (React or SSR depending on your setup)
+router.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+// SSR Highscore page
+router.get("/highscore-ssr", async (req, res) => {
+  const scores = await getAllScores(); // hämtar från SQLite
+  res.render("highscore", { scores });
 });
 
 export default router;
