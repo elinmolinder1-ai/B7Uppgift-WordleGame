241c6c9bdd7243b97c513824ac0fd58d200992c6
elinmolindr <elinmolinder1@gmail.com>
2026-04-07 16:23:58 +0200

    Create Highscore schema and model for storing game results

--- backend/models/Highscore.js
+++ backend/models/Highscore.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-const highscoreSchema = new mongoose.Schema({
-  name: String,
-  timeTaken: Number,
-  wordLength: Number,
-  uniqueLetters: Boolean,
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+//vEach field describes what data a highscore must contain
+const HighscoreSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  timeTaken: { type: Number, required: true },
+  guesses: { type: Array, required: true },
+  wordLength: { type: Number, required: true },
+  uniqueLetters: { type: Boolean, required: true },
+  word: { type: String, required: true }
 });
 
-export const Highscore = mongoose.model("Highscore", highscoreSchema);
+export const Highscore = mongoose.model("Highscore", HighscoreSchema);
