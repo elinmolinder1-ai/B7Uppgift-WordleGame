4fc767fe26516e68dd63381a30c7b41a12ad85e3
elinmolindr <elinmolinder1@gmail.com>
2026-04-07 16:23:48 +0200

    Add MongoDB connection function using Mongoose

--- backend/database/mongoose.js
+++ backend/database/mongoose.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
 
+// Connects the backend to the local MongoDB database using Mongoose.
 export async function connectDB() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/wordle", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect("mongodb://127.0.0.1:27017/wordle");
 
     console.log("Connected to MongoDB");
   } catch (err) {
